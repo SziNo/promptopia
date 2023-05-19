d2aaec9de96e9663d13105a555f035d3aeeb833a
SziNo <szino1848@gmail.com>
2023-05-19 11:50:19 +0200

    update create-prompt page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+'use client'
+
+import { useState, useCallback } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import Form from '@components/Form'
 
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>
+  const [submitting, setSubmitting] = useState(false)
+  const [post, setPost] = useState({ prompt: '', tag: '' })
+
+  const router = useRouter()
+  const { data: session } = useSession()
+
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault()
+      setSubmitting(true)
+
+      try {
+        const response = await fetch('/api/prompt/new', {
+          method: 'POST',
+          body: JSON.stringify({
+            prompt: post.prompt,
+            userId: session?.user.id,
+            tag: post.tag,
+          }),
+        })
+
+        if (response.ok) {
+          router.push('/')
+        }
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setSubmitting(false)
+      }
+    },
+    [post.prompt, session?.user.id, post.tag]
+  )
+
+  return (
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={handleSubmit}
+    ></Form>
+  )
 }
 
 export default CreatePrompt
